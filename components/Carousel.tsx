8c8de7eba979aab7d68feb0e49ae788cc0c38d75
Koayniaina <setraniaina08@gmail.com>
2025-07-27 13:19:00 +0300

    done

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Carousel() {
   return (
     <div>
-      <div
+      {/* <div
         id="carouselExampleSlidesOnly"
         className="carousel slide"
         data-bs-ride="carousel"
@@ -18,9 +18,6 @@ export default function Carousel() {
             />
             <div className="carousel-caption ">
               <h5>Second slide label</h5>
-              {/* <span>
-                Some representative placeholder content for the second slide.
-              </span> */}
             </div>
           </div>
           <div className="carousel-item ciner active">
@@ -32,40 +29,116 @@ export default function Carousel() {
             />
             <div className="carousel-caption ">
               <h5>Second slide label</h5>
-              {/* <span>
-                Some representative placeholder content for the second slide.
-              </span> */}
+           
             </div>
           </div>
           <div className="carousel-item ciner">
             <img
               src="https://images.unsplash.com/photo-1609692814858-f7cd2f0afa4f?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="d-block w-100 cimg"
-              // height="300px"
+             
               alt="..."
             />
             <div className="carousel-caption">
               <h5>Second slide label</h5>
-              {/* <span>
-                Some representative placeholder content for the second slide.
-              </span> */}
+            
             </div>
           </div>
           <div className="carousel-item  ciner">
             <img
               src="https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               className="d-block w-100 cimg"
-              // height="300px"
+             
               alt="..."
             />
             <div className="carousel-caption ">
               <h5>Second slide label</h5>
-              {/* <span>
-                Some representative placeholder content for the second slide.
-              </span> */}
+             
             </div>
           </div>
         </div>
+      </div> */}
+      <div id="carouselExampleIndicators" className="carousel slide">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item   ciner active">
+             <img
+              src="https://images.unsplash.com/photo-1510698453013-dd490a55cb4e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="d-block w-100 cimg"
+              height="300px"
+              alt="..."
+            />
+            <div className="carousel-caption ">
+              <h5>Second slide label</h5>
+            </div>
+          </div>
+          <div className="carousel-item ciner">
+            <img
+              src="https://images.unsplash.com/photo-1593121925328-369cc8459c08?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="d-block w-100 cimg"
+              height="300px"
+              alt="..."
+            />
+            <div className="carousel-caption ">
+              <h5>Second slide label</h5>
+            </div>
+          </div>
+          <div className="carousel-item  ciner">
+            <img
+              src="https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="d-block w-100 cimg"
+              alt="..."
+            />
+            <div className="carousel-caption ">
+              <h5>Second slide label</h5>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -1,5 +1,64 @@
 import React from "react";
 
 export default function Category() {
-  return <div className="very">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, natus, quaerat rem ad nam ducimus incidunt labore consequatur fuga omnis, autem vel quia quisquam tenetur voluptatum? Harum veritatis Consectetur nisi rerum fuga autem veritatis molestiae libero, molestias, natus, blanditiis eaque ipsa sequi ut</div>;
+  return (
+    <div>
+      <h6 className="text-gray-700 p-4">Upcoming</h6>
+      <div className="cardflex">
+        <div className="card" style={{ width: "18rem",  margin: "1rem" }}>
+          <img
+            src="https://img.pccomponentes.com/articles/1087/10879424/12-pc-sobremesa-hp-27-cr1010ns-intel-core-ultra-5-125u-16gb-512gb-ssd-27-fhd-win11-especificaciones.jpg"
+            className="card-img-top p-7 h-[60%] "
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+            {/* <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a> */}
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem", margin: "1rem" }}>
+          <img
+            src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/desktops/optiplex-desktops/optiplex-d13-tower/13-5-pdp/desktop-optiplex-d13-5-7020-tower-alexandria-plus-sustainability.psd?wid=950&hei=781&fmt=png-alpha&qlt=100%2C0&op_usm=1.75%2C0.3%2C2%2C0&resMode=sharp2&pscan=auto&fit=constrain%2C1&align=0%2C0"
+            className="card-img-top p-7 h-[60%]"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+            {/* <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a> */}
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem", margin: "1rem" }}>
+          <img
+            src="https://www.tunisianet.com.tn/img/cms/3280AIO/desktop-inspiron-22-3280-aio-emea-direct-pdp-3.jpg"
+            className="card-img-top p-7 h-[60%]"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+            {/* <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a> */}
+          </div>
+        </div>
+       
+
+      </div>
+    </div>
+  );
 }
